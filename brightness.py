ac30f359d0cfb545896aa8de320202b0127098a7
marco5de <marco.deuscher@t-online.de>
2019-01-30 12:23:55 +0100

    brightness script

---
+++ brightness.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import sys
+
+if(len(sys.argv)!=2):
+    print("Usage: brightness [+/.:brightness]")
+    sys.exit()
+
+
+MIN=500
+MAX=7500
+curr_abs=0
+curr_rel=0
+
+
+filename="/sys/class/backlight/intel_backlight/brightness"
+file=open(filename,"r")
+for line in file:
+    curr_abs=int(line)
+
+curr_rel=curr_abs/MAX*100
+file.close()
+
+u_input = int(sys.argv[1][1:])*7500/100
+if(sys.argv[1][0] == '+'):
+    new_abs=curr_abs+u_input 
+elif(sys.argv[1][0] == '-'):
+    new_abs=curr_abs-u_input
+else:
+    print("Usage: brightness [+/-:brigthness]")
+    sys.exit()
+
+if(not(new_abs >= MIN and new_abs <= MAX)):
+    print("Argument to large: brightness has to be between 500 and 7500 abs")
+    sys.exit()
+
+n_file=open(filename,"w")
+n_file.write('%d' % new_abs)
+n_file.close()
+
