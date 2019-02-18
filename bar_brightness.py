96886b2d598e84f85af18bb837b10c1970c8339b
marco5de <marco.deuscher@t-online.de>
2019-02-18 18:57:28 +0100

    script to display brightness on bar with i3blocks

---
+++ bar_brightness.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import sys
+
+MAX=7500
+
+filename="/sys/class/backlight/intel_backlight/brightness"
+file=open(filename,"r")
+curr_abs=file.readline()
+file.close()
+
+rel=int(curr_abs)/MAX*100
+print(str(rel) + "%")
+
+
